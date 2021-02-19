0ee622eb7311f2cdb1722092c34845ca2e863065
itibanbosi <51027729+itibanbosi@users.noreply.github.com>
2021-02-19 15:36:20 +0900

    Add files via upload

--- test.ts
+++ test.ts
@@ -1,1 +1,3 @@
-// tests go here; this will not be compiled when this package is used as an extension.
+basic.forever(function () {
+    basic.showString("" + (stem.TP2_getTemperature(OutputNumberFormat.FLOAT)))
+})
